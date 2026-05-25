2e46a0262875be568de87fbc38a8b9264248cbcb
Amjad Khan <amjad22558@gmail.com>
2026-05-25 22:34:46 +0300

    added PATCH to cors

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(
       "http://localhost:5173",
       "https://booking-system-xi-three.vercel.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
